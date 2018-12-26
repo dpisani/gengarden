23a4c5bb54c48bbed4b12eadb03f8a24abc6afd8
Dean Pisani <dean.a.pisani@gmail.com>
2018-12-26 13:14:25 +1100

    Add branch generator which builds basic cylinders

---
+++ src/generators/branch/index.ts
@@ -0,0 +1,66 @@
+import {
+  Node,
+  Mesh,
+  Primitive,
+  buildIndices,
+  buildPosition,
+} from 'gltf-builder';
+import { vec3 } from 'gl-matrix';
+import { flatten } from 'lodash';
+
+interface BranchSpec {
+  begin: vec3;
+  end: vec3;
+  width: number;
+}
+
+export default (spec: BranchSpec) => {
+  const segments = 10;
+  const segmentAngle = (Math.PI * 2) / segments;
+  const ring: vec3[] = Array.from({ length: segments }).map((_x, i) => {
+    const a = segmentAngle * i;
+    return vec3.fromValues(Math.cos(a), 0, Math.sin(a));
+  });
+
+  const topRing = ring.map(p =>
+    vec3.add(
+      vec3.create(),
+      vec3.scale(vec3.create(), p, spec.width),
+      spec.begin,
+    ),
+  );
+  const bottomRing = ring.map(p =>
+    vec3.add(vec3.create(), vec3.scale(vec3.create(), p, spec.width), spec.end),
+  );
+
+  // add an extra element for wrap around
+  const topRingIndexes: number[] = [
+    ...Array.from({ length: segments }).map((_x, i) => i),
+    0,
+  ];
+  const bottomRingIndexes: number[] = [
+    ...Array.from({ length: segments }).map((_x, i) => i + segments),
+    segments,
+  ];
+
+  const quads = Array.from({ length: segments }).map((_x, i) => {
+    return [
+      bottomRingIndexes[i],
+      topRingIndexes[i + 1],
+      topRingIndexes[i],
+
+      bottomRingIndexes[i],
+      bottomRingIndexes[i + 1],
+      topRingIndexes[i + 1],
+    ];
+  });
+
+  const positions = buildPosition([...topRing, ...bottomRing]);
+  const indices = buildIndices(flatten(quads));
+
+  return new Node().mesh(
+    new Mesh().addPrimitive(
+      new Primitive().indices(indices).position(positions),
+    ),
+  );
+};
---
+++ src/samples/branches.ts
@@ -0,0 +1,17 @@
+import { Asset, Scene } from 'gltf-builder';
+import { vec3 } from 'gl-matrix';
+import * as fs from 'fs';
+
+import generateBranch from '../generators/branch';
+
+const branch = generateBranch({
+  begin: vec3.fromValues(0, 0, 0),
+  end: vec3.fromValues(0, 1, 0),
+  width: 0.2,
+});
+const asset = new Asset().addScene(new Scene().addNode(branch));
+
+const gltf = JSON.stringify(asset.build(), null, 2);
+
+fs.mkdirSync('generated', { recursive: true });
+fs.writeFileSync('generated/branch.gltf', gltf);
