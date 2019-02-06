fcd775495b0eac65a1bc98a5642703cd7800db56
Dean Pisani <dean.a.pisani@gmail.com>
2019-02-06 15:06:35 +1100

    Fix branching calculation used by tree generator

--- src/generators/tree/index.ts
+++ src/generators/tree/index.ts
@@ -1,4 +1,4 @@
-import { vec3, mat4 } from 'gl-matrix';
+import { vec3, quat } from 'gl-matrix';
 import { prng } from 'seedrandom';
 import { flatMap } from 'lodash';
 import { Node } from 'gltf-builder';
@@ -26,6 +26,36 @@ const isValidSpec = (
   return type === 'tree';
 };
 
+const createDeviation = (
+  originalDirection: vec3,
+  minAngle: number,
+  maxAngle: number,
+  rng: prng,
+): vec3 => {
+  const chosenAngle = (maxAngle - minAngle) * rng() + minAngle;
+
+  // create a vector deviating from the Y axis and then rotate it towards the original direction
+  const deviated = vec3.fromValues(
+    Math.sin(chosenAngle),
+    Math.cos(chosenAngle),
+    0,
+  );
+
+  const qRotateY = quat.create();
+  quat.rotateY(qRotateY, qRotateY, Math.PI * 2 * rng());
+
+  const qToOriginal = quat.rotationTo(
+    quat.create(),
+    vec3.fromValues(0, 1, 0),
+    originalDirection,
+  );
+
+  vec3.transformQuat(deviated, deviated, qRotateY);
+  vec3.transformQuat(deviated, deviated, qToOriginal);
+
+  return deviated;
+};
+
 const generateBranches = (
   startPos: vec3,
   startDir: vec3,
@@ -44,24 +74,29 @@ const generateBranches = (
   }[] = [];
 
   const currentPos = vec3.clone(startPos);
+  const currentDir = vec3.clone(startDir);
 
   let generatedPoints = 0;
   do {
-    vec3.add(currentPos, currentPos, startDir);
+    vec3.add(currentPos, currentPos, currentDir);
     generatedPoints++;
 
-    if (rng() < 0.2) {
+    if (rng() < 0.05 * maxDepth) {
       const currentWidth = (1 - generatedPoints / maxDepth) * startWidth;
 
       // fork
-      const r = 0.1;
-      const rotation = mat4.fromXRotation(mat4.create(), r);
+
       if (maxDepth - generatedPoints > 0) {
         forks.push({
           startPos: vec3.clone(currentPos),
-          direction: vec3.transformMat4(vec3.create(), startDir, rotation),
+          direction: createDeviation(
+            currentDir,
+            Math.PI * 0.05,
+            Math.PI * 0.25,
+            rng,
+          ),
           maxDepth: maxDepth - generatedPoints,
-          startWidth: currentWidth,
+          startWidth: currentWidth * 0.8,
         });
       }
 
@@ -69,6 +104,12 @@ const generateBranches = (
         position: vec3.clone(currentPos),
         width: currentWidth,
       });
+
+      //wiggle the direction of the main branch
+      vec3.copy(
+        currentDir,
+        createDeviation(currentDir, 0, Math.PI * 0.25, rng),
+      );
     }
   } while (generatedPoints < maxDepth && rng() > 0.01);
 
